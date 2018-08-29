8d2edbe2525161c2ca17283d81434e46b9b17d5e
Teófilo Duffau <tduffau@itba.edu.ar>
2018-08-29 11:46:02 -0300

    Edited laser

--- Astroball/Assets/Scripts/ShipCollisionDetection.cs
+++ Astroball/Assets/Scripts/ShipCollisionDetection.cs
@@ -3,25 +3,49 @@ using System.Collections;
 
 public class ShipCollisionDetection : MonoBehaviour {
 
-     public GameObject elasticRope;
+    public GameObject elasticRope;
+
+    private int playerNumber;
+
+    private void Start() {
+        this.playerNumber = this.gameObject.GetComponent<ShipData>().playerNumber;
+    }
 
     void OnCollisionEnter2D(Collision2D other){
 
-        if (other.gameObject.tag == "Ball"){
+        if (other.gameObject.tag == "Ball" || other.gameObject.tag == "Chain"){
+            if (this.HasTheBall()) return;
+            this.StealBall();   
+        } else if(other.gameObject.tag == "Meteor" || other.gameObject.tag == "Laser"){
+            LoseTheBall();
+        }
+        // else if(other.gameObject.tag == "Player") {
+        //     if (GameManagerScript.instance.PlayerWithBall == other.gameObject.GetComponent<ShipData>().playerNumber){
+        //         StealBall(GameObject.FindGameObjectWithTag("Ball"));
+        //     }
+        // }
+    }
 
-            if (GameManagerScript.instance.PlayerWithBall == this.gameObject.GetComponent<ShipData>().playerNumber) return;
-            //GameObject elasticRopeGameObj = GameObject.Find("ElasticRope");
-            GameObject rope = GameObject.FindGameObjectWithTag("Chain");
-            if(rope != null) rope.GetComponent<ElasticRope>().DisconnectRope();
+    void StealBall(){
+        GameObject rope = GameObject.FindGameObjectWithTag("Chain");
+        if (rope != null) rope.GetComponent<ElasticRope>().DisconnectRope();
 
-            GameManagerScript.instance.PlayerWithBall = this.gameObject.GetComponent<ShipData>().playerNumber;
-            rope = Instantiate(elasticRope);
-            rope.transform.position = other.gameObject.transform.position;
-            rope.GetComponent<ElasticRope>().startPoint = this.gameObject;
-            rope.GetComponent<ElasticRope>().endPoint = other.gameObject;
+        GameManagerScript.instance.PlayerWithBall = this.gameObject.GetComponent<ShipData>().playerNumber;
+        rope = Instantiate(elasticRope);
+        rope.transform.position = GameManagerScript.instance.ball.transform.position;
+        rope.GetComponent<ElasticRope>().startPoint = this.gameObject;
+        rope.GetComponent<ElasticRope>().endPoint = GameManagerScript.instance.ball.gameObject;
+    }
 
+    bool HasTheBall(){
+        return GameManagerScript.instance.PlayerWithBall == this.playerNumber;
+    }
 
-            // Destroy(other.gameObject);
+    void LoseTheBall(){
+        if (this.HasTheBall()){
+            GameObject.FindGameObjectWithTag("Chain").GetComponent<ElasticRope>().DisconnectRope();
         }
     }
+
+
 }
