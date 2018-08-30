06d3b417bd4d62fdb2547fb98c7ef8d96826fd8b
Teófilo Duffau <tduffau@itba.edu.ar>
2018-08-29 21:16:56 -0300

    Changes to the project structure

--- Astroball/Assets/Scripts/StayLoopOnScreen.cs
+++ Astroball/Assets/Scripts/StayLoopOnScreen.cs
@@ -20,15 +20,19 @@ public class StayLoopOnScreen : MonoBehaviour {
 	void Update () {
         if (render.isVisible)
         {
-            if (transform.position.x > camHalfWidth + offsetX)
+            if (transform.position.x > camHalfWidth + offsetX){
                 transform.position = new Vector2((-camHalfWidth - offsetX) + 0.0001f , transform.position.y);
-            else if (transform.position.x < -camHalfWidth - offsetX)
+            }
+            else if (transform.position.x < -camHalfWidth - offsetX){
                 transform.position = new Vector2((camHalfWidth + offsetX) - 0.0001f, transform.position.y);
+            }
 
-            if (transform.position.y > camHalfHeight + offsetY)
+            if (transform.position.y > camHalfHeight + offsetY){
                 transform.position = new Vector2(transform.position.x, (-camHalfHeight - offsetY) + 0.0001f);
-            else if (transform.position.y < -camHalfHeight - offsetY)
+            }
+            else if (transform.position.y < -camHalfHeight - offsetY){
                 transform.position = new Vector2(transform.position.x, (camHalfHeight + offsetY) - 0.0001f);
+            }
         }
     }
 }
